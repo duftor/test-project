89cc7d07ca31679b738bf01688766f05973dee01
Victor Dufayet <duftor@outlook.fr>
2023-02-17 23:16:55 +0100

    ♻️ Change IP address

--- App.js
+++ App.js
@@ -5,64 +5,62 @@ import { Button, StyleSheet, Text, View } from "react-native"
 import socket from "./utils/socket"
 
 const protocol = "http"
-const ipAddr = "192.168.1.50"
+const ipAddr = "192.168.56.1"
 const port = "8080"
-const url = protocol + "://" + ipAddr + ":" + port + "/data/"
+const url = protocol + "://" + ipAddr + ":" + port + "/data/get"
 
 export default function App() {
-    const [data, setData] = useState([])
+	const [data, setData] = useState([])
 
-    const getUserAccount = () => {
-        socket.emit("test", 10)
-        socket.on("resTest", () => console.log("RESPONSE TEST"))
+	const getUserAccount = () => {
+		socket.emit("test", 10)
+		socket.on("resTest", () => console.log("RESPONSE TEST"))
 
-        axios
-            .get(url)
-            .then(function (response) {
-                setData(response.data)
-                // console.log("Response : ", response.data)
-            })
-            .catch(function (error) {
-                console.log(error)
-            })
-    }
+		axios
+			.get(url)
+			.then(function (response) {
+				setData(response.data)
+				// console.log("Response : ", response.data)
+			})
+			.catch(function (error) {
+				console.log(error)
+			})
+	}
 
-    return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
-            <Button onPress={getUserAccount} title="Get user account" />
-            <View style={styles.viewData}>
-                {data.map((nodeData) => (
-                    <View style={styles.data}>
-                        <Text>
-                            ----------------------------------------------
-                        </Text>
-                        <Text>NodeID : {nodeData.nodeId}</Text>
-                        <Text>GatewayId : {nodeData.gatewayId}</Text>
-                        <Text>RSSI : {nodeData.rssi}</Text>
-                        <Text>Temperature : {nodeData.temperature}</Text>
-                        <Text> Humidity : {nodeData.humidity}</Text>
-                        <Text>Noise : {nodeData.noise}</Text>
-                    </View>
-                ))}
-            </View>
-        </View>
-    )
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.js to start working on your app!</Text>
+			<StatusBar style="auto" />
+			<Button onPress={getUserAccount} title="Get user account" />
+			<View style={styles.viewData}>
+				{data.map((nodeData) => (
+					<View style={styles.data}>
+						<Text>----------------------------------------------</Text>
+						<Text>NodeID : {nodeData.nodeId}</Text>
+						<Text>GatewayId : {nodeData.gatewayId}</Text>
+						<Text>RSSI : {nodeData.rssi}</Text>
+						<Text>Temperature : {nodeData.temperature}</Text>
+						<Text> Humidity : {nodeData.humidity}</Text>
+						<Text>Noise : {nodeData.noise}</Text>
+					</View>
+				))}
+			</View>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    viewData: {
-        flexDirection: "column",
-    },
-    data: {
-        // backgroundColor: "red",
-        // padding: "4",
-    },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	viewData: {
+		flexDirection: "column",
+	},
+	data: {
+		// backgroundColor: "red",
+		// padding: "4",
+	},
 })
--- utils/socket.js
+++ utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client"
 
-const socket = io.connect("http://192.168.1.50:8080")
+const socket = io.connect("http://192.168.56.1:8080")
 
 export default socket
 
